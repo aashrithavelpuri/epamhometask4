7369ab8f122c5a912156f330d51612c5c144e5c0
aashrithavelpuri <60144824+aashrithavelpuri@users.noreply.github.com>
2020-02-12 18:25:56 +0530

    Add files via upload

---
+++ home_task_4/src/main/java/home_task_4/HouseConstructionCostEstimationMain.java
@@ -0,0 +1,10 @@
+package home_task_4;
+
+class HouseConstructionCostEstimationMain
+{
+	public static void main(String args[])
+	{
+		HouseConstructionCostEstimation estimate = new HouseConstructionCostEstimation();
+		estimate.costPerSquareFeet();
+	}
+}
---
+++ home_task_4/src/main/java/home_task_4/SimpleCompoundInterestMain.java
@@ -0,0 +1,8 @@
+package home_task_4;
+
+public class SimpleCompoundInterestMain extends SimpleCompoundInterest{
+	public static void main(String args[]) {
+	    SimpleCompoundInterest logic = new SimpleCompoundInterest();
+	    logic.calculate();
+	}
+}
