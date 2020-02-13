74d6f913e46422232fe8edec7d6511b96f0d3d64
aashrithavelpuri <60144824+aashrithavelpuri@users.noreply.github.com>
2020-02-13 15:45:52 +0530

    Update SimpleCompoundInterest.java

--- home_task_4/src/main/java/home_task_4/SimpleCompoundInterest.java
+++ home_task_4/src/main/java/home_task_4/SimpleCompoundInterest.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class SimpleCompoundInterest  {
 	public void calculate()
 	{
-		double principalAmount = 0.0, simpleInterest = 0.0,compoundInterest = 0.0;
-		int numberOfTimes = 0,rateOfInterest = 0, time = 0;
+		double principalAmount = 0.0, simpleInterest = 0.0,compoundInterest = 0.0,,rateOfInterest = 0.0, time = 0.0;
+		int numberOfTimes = 0;
 	    	Scanner sc=new Scanner (System. in);
 	    	System.out.printf("Enter the principal amount:");
 	    	principalAmount = sc.nextDouble();
-	    	System. out. printf("Enter the time(in years):");
-	    	time = sc.nextInt();
+	    	System. out. printf("Enter the period(in years):");
+	    	time = sc.nextDouble();
 	    	System. out. printf("Enter the Rate of  interest:");
-	    	rateOfInterest=sc.nextInt();
+	    	rateOfInterest=sc.nextDouble();
 	    	System.out.printf("Enter the number of times:");
 	    	numberOfTimes=sc.nextInt();
 		simpleInterest=(principalAmount * time * rateOfInterest)/100;
